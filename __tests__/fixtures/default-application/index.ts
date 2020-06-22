e96deeaa62f0226d5b7f7810fa0db616fd7a61fb
XiongZhikai <xiongzhikai@icloud.com>
2020-06-22 12:27:20 +0800

    Update Test Applicattion HOST and PORT

--- __tests__/fixtures/default-application/index.ts
+++ __tests__/fixtures/default-application/index.ts
@@ -2,7 +2,18 @@ import { ApplicationConfig } from '@loopback/core';
 import { ExpectFunctionApplication } from './application';
 
 export async function DefaultMain(options: ApplicationConfig = {
-    shutdown: { signals: ['SIGINT'] }
+    shutdown: { signals: ['SIGINT'] },
+    rest: {
+        port: +(process.env.PORT || 6006),
+        host: process.env.HOST,
+        openApiSpec: {
+            setServersFromRequest: false,
+        }
+    },
+    websocket: {
+        port: +(process.env.PORT || 6016),
+        host: process.env.HOST
+    }
 }) {
 
     const app = new ExpectFunctionApplication(options);
